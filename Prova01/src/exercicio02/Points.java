637e7884cf33aee3533be856b918233419df4d01
Márcio <marciov.dasilva@gmail.com>
2017-03-21 19:11:00 -0300

    teste

--- Prova01/src/exercicio02/Points.java
+++ Prova01/src/exercicio02/Points.java
@@ -33,6 +33,7 @@ public static void main(String[] args) {
     JFrame frame = new JFrame("Points");
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.add(points);
+    ///
     frame.setSize(250, 200);
     frame.setLocationRelativeTo(null);
     frame.setVisible(true);
